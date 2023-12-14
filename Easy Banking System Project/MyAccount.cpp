9d1a8e3c283be569c09e87bc55ce1c1f7ab10d8e
matei1998 <153720444+matei19989@users.noreply.github.com>
2023-12-14 23:47:11 +0200

    Add files via upload

--- Easy Banking System Project/MyAccount.cpp
+++ Easy Banking System Project/MyAccount.cpp
@@ -3,54 +3,61 @@
 #include <fstream>
 #include <stdexcept>
 #include<string>
-MyAccount::MyAccount(){}
-MyAccount:: MyAccount(double money, double debt, std::string currency): Money(money), Debt(debt), Currency(currency) {
-        Currencies.push_back("Euros");
-        Currencies.push_back("Dollars");
-        Currencies.push_back("Pounds");
-        Currencies.push_back("Leu");
-        if (money < 0) {
-            throw std::runtime_error("Initial money cannot be lower than 0");
-        }
+
+MyAccount::MyAccount() {}
+MyAccount::MyAccount(double money, double debt, std::string currency): Money(money), Debt(debt), Currency(currency) {
+    Currencies.push_back("Euros");
+    Currencies.push_back("Dollars");
+    Currencies.push_back("Pounds");
+    Currencies.push_back("Leu");
+    if (money < 0) {
+        throw std::runtime_error("Initial money cannot be lower than 0");
     }
-double MyAccount:: CheckDebt()const{
+}
+double MyAccount::CheckDebt() const {
     return Debt;
 }
-void MyAccount ::displayBalance() const{
-   std::cout << "Available money in the bank is " << Money << '\n';
+void MyAccount::displayBalance() const {
+    std::cout << "Available money in the bank is " << Money << '\n';
 }
-void MyAccount::Deposit(double amount){
-Money += amount;
+void MyAccount::Deposit(double amount) {
+    Money += amount;
 }
-void MyAccount::Withdraw(double amount){
-        Money -= amount;
+void MyAccount::Withdraw(double amount) {
+    Money -= amount;
 
 }
-std::string MyAccount ::CheckCurrency() const{
-        return Currency;
+std::string MyAccount::CheckCurrency() const {
+    return Currency;
 
 }
-double MyAccount::addDebt(double amount){
-        return Debt += amount;
+double MyAccount::addDebt(double amount) {
+    return Debt += amount;
 
 }
-void MyAccount::FileNumbers(const std::string & date){
-std::ifstream file(date);
+void MyAccount::FileNumbers(const std::string & date) {
+    try {
+        std::ifstream file(date);
         if (file.is_open()) {
             file >> Money >> Debt >> Currency;
             file.close();
         } else {
             throw std::runtime_error("Failed to open file: " + date);
         }
+    } catch (const std::exception & e) {
+        std::cerr << "File not found" << e.what() << '\n';
+    }
 }
-void MyAccount::savedNumbers(const std::string & date){
-std::ofstream file(date);
+void MyAccount::savedNumbers(const std::string & date) {
+    try {
+        std::ofstream file(date);
         if (file.is_open()) {
             file << Money << " " << Debt << " " << Currency;
             file.close();
         } else {
             throw std::runtime_error("Failed to open file: " + date);
         }
+    } catch (const std::exception & e) {
+        std::cerr << "File not found" << e.what() << '\n';
+    }
 }
-
-
